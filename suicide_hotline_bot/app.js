292355f163b85fae16217adadb48317429208182
Christophe Asselin <christophe.asselin@hotmail.com>
2020-01-18 14:34:31 -0500

    added tweet api  and analyze fct interval

---
+++ suicide_hotline_bot/app.js
@@ -0,0 +1,25 @@
+const Twitter = require('twitter');
+ 
+const client = new Twitter({
+  consumer_key: 'mtYELil25BefjifvozfvhrRuH',
+  consumer_secret: 'khdQHcnAEq4WCNtxYHOiXLbe273b4UHparM0J0bZIxWDk8VK15',
+  access_token_key: '2276885120-GAM47V8s1IyJ4mtsjRAN2H6WX74ZuLhAoLnp8Sy',
+  access_token_secret: '5FYn61Oi7WlleGCkwS0zY4F45YezIkEMLi5fI1jQ7Pyww'
+});
+
+let tweet_array = [];
+const MONTREAL_BOUNDING_BOX = '-73.9058,45.4146,-73.4769,45.7029';
+const params = {locations: MONTREAL_BOUNDING_BOX}
+const stream = client.stream('statuses/filter', params);
+
+stream.on('data', (event) => {
+    tweet_array.push(event);
+});
+ 
+analyzeTweets = async () => {
+    const tweets = [...tweet_array];
+    tweet_array = [];
+    console.log(tweets.length);
+};
+
+setInterval(() => analyzeTweets(), 60000);
